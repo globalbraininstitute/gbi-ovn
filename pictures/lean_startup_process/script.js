2ed6df040f2a5bd3a77a6ed19efb5a7080d4f741
vveitas <vveitas@gmail.com>
2015-12-03 19:13:18 +0100

    faster animation on the slide with custom js scipt

--- pictures/lean_startup_process/script.js
+++ pictures/lean_startup_process/script.js
@@ -1,9 +1,9 @@
 animate = function() {
- d3.select(this).transition().duration(1500)
+ d3.select(this).transition().duration(500)
    .attr("fill", "red");
 }    
 animateback = function() {
- d3.select(this).transition().duration(1)
+ d3.select(this).transition().duration(500)
    .attr("fill", "white");
 }
 
@@ -15,45 +15,45 @@ function toggleStep() {
         counter++;
         switch (counter) {
         case 1:
-          d3.select("#circle56").transition().duration(1000)
+          d3.select("#circle56").transition().duration(400)
               .attr("fill", "white").each("end", animate);
           break;
         case 2:
-          d3.select("#circle50").transition().duration(1000)
+          d3.select("#circle50").transition().duration(400)
               .attr("fill", "white").each("end", animate);
           break;
         case 3:
-          d3.select("#circle44").transition().duration(1000)
+          d3.select("#circle44").transition().duration(400)
               .attr("fill", "white").each("end", animate);
           break;
         case 4:
-          d3.select("#circle50").transition().duration(1000)
+          d3.select("#circle50").transition().duration(400)
               .attr("fill", "red").each("end", animateback);
-          d3.select("#circle50").transition().duration(1000)
+          d3.select("#circle50").transition().duration(400)
               .attr("fill", "white").each("end", animate);
           break;
         case 5:
-          d3.select("#circle38").transition().duration(1000)
+          d3.select("#circle38").transition().duration(400)
               .attr("fill", "white").each("end", animate);
           break;
         case 6:
-            d3.select("#circle50").transition().duration(1000)
+            d3.select("#circle50").transition().duration(400)
               .attr("fill", "red").each("end", animateback);
-            d3.select("#circle50").transition().duration(1000)
+            d3.select("#circle50").transition().duration(400)
               .attr("fill", "white").each("end", animate);
           break;
         case 7:
-            d3.select("#circle56").transition().duration(1000)
+            d3.select("#circle56").transition().duration(400)
               .attr("fill", "red").each("end", animateback);
-            d3.select("#circle56").transition().duration(1000)
+            d3.select("#circle56").transition().duration(400)
               .attr("fill", "white").each("end", animate);
           break;
         case 8:
-            d3.select("#circle32").transition().duration(1000)
+            d3.select("#circle32").transition().duration(400)
               .attr("fill", "white").each("end", animate);
           break;
         case 9:
-            d3.select("#circle56-9").transition().duration(1000)
+            d3.select("#circle56-9").transition().duration(400)
               .attr("fill", "white").each("end", animate);
           break;
 
